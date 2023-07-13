b2334040894b0ab9931051d3565bc06240f41862
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-07-13 16:03:58 -0400

    controller

--- pokedex-rails-backend/config/routes.rb
+++ pokedex-rails-backend/config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: {format: :json} do 
+get "pokemon/types", to: "pokemon#types"
+resources :items , only: [:update, :destroy ]
+resources :pokemon , only: [:show, :index ,:create ,:update]do 
+resources :items , only: [:index,:create]
+# resources :types , only: [:index]
+
+  end
+  end
 end
